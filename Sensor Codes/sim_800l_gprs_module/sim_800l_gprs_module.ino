1eb66f0cf5775b29580763f59d42607168b88487
AKSHILMY <akshilmy@gmail.com>
2022-06-14 13:23:27 +0530

    nodemcu wifi module

--- Sensor Codes/sim_800l_gprs_module/sim_800l_gprs_module.ino
+++ Sensor Codes/sim_800l_gprs_module/sim_800l_gprs_module.ino
@@ -1,28 +1,51 @@
-/*
-***************************************************
-Programmed by iakbasnayaka for www.setnfix.com
-This is for checking of AT commands for SIM800L
-https://www.youtube.com/channel/UCM9NQ4WcSbzTiCdoyOvzA1g
-***************************************************
-*/
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 
-#include <SoftwareSerial.h>
-SoftwareSerial Sim800l(10,11);
-void setup() {
-  // put your setup code here, to run once:
- Serial.begin(9600);
-  Sim800l.begin(9600);
+const char* ssid = "AKS";
+const char* password = "987654321";
+
+ ESP8266WebServer server(80);
+
+ String page = "";
+ int LEDPin = 2;
+
+ void setup(){
+  page="<h1>Simple NODEMCU WEB SERVER</h1><p><a href=\"LEDOn\"><button>ON</button></a>&nbsp;<a href=\"LEDOff\"><button>OFF</button></a></p>";
+  pinMode(LEDPin,OUTPUT);
+  digitalWrite(LEDPin,LOW);
+
+  delay(1000);
+  Serial.begin(115200);
+  WiFi.begin(ssid,password);
+  Serial.println("");
+
+
+ while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+ }
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.println("IP address ");
+    Serial.println(WiFi.localIP());
+ server.on("/",[](){
+  server.send(200,"text/html",page);   
+ });
+ server.on("/LEDOn",[](){
+  server.send(200,"text/html",page);  
+  digitalWrite(LEDPin,LOW);
+  delay(1000);
+  });
+ server.on("/LEDOff",[](){
+  server.send(200,"text/html",page);  
+  digitalWrite(LEDPin,HIGH);
+  delay(1000);
+  });
+  server.begin();
+  Serial.println("Web Server Started!");
 }
-
-void loop() {
- 
- while(Sim800l.available()){
-  Serial.write(Sim800l.read());
- }
-
-  
- while(Serial.available()){
-  Sim800l.write(Serial.read());
- }
-
+void loop(void){
+  server.handleClient();
 }
