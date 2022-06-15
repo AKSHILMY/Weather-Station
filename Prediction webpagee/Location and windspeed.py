c86becce5e31d2008faa7572be9a68e513152b02
hafeelnm19 <hafeelnm.19@uom.lk>
2022-06-15 13:35:28 +0530

    Location and wind speed

---
+++ Prediction webpagee/Location and windspeed.py
@@ -0,0 +1,23 @@
+import requests
+import os
+from datetime import datetime
+import json
+from urllib.request import urlopen
+url="http://ipinfo.io/json"
+response=urlopen(url)
+data=json.load(response)
+
+user_api = "fe4a36dc39d229db5428ec9b0beb4278"
+location = data['city']
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+user_api
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+print ("-------------------------------------------------------------")
+print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("-------------------------------------------------------------")
+
+print ("Current wind speed    :",wind_spd ,'kmph')
